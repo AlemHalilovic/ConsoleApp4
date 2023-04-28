d34b6529edc7a65d60e24fc86470d98e2ce83beb
wahoolie <vino04003@utb.vaxjo.se>
2023-04-28 11:24:56 +0200

    Add project files.

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+     class Class1
+     {
+        private int vikt;
+        private string märke;
+        private int årsmodell;
+        public string mymetod_1
+        {
+            get {
+                return märke;
+            }
+            set {
+                märke= value;
+
+            }
+
+          
+        }
+        public int mymetod_2
+        {
+            get
+            {
+                return årsmodell;
+            }
+            set{
+                årsmodell= value;
+            }
+
+
+        }
+        public int mymetod_3
+        {
+            get { return vikt;} 
+            set{
+                vikt= value;
+            }
+        }
+
+            
+
+
+
+
+
+
+
+    }
+
+
+}
+
+   
+    
+
+        
+    
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace ConsoleApp4
+{
+    class program
+    {
+        static void Main()
+        {
+             Class1 bil1 = new Class1();
+            bil1.mymetod_3 = 3500;
+            bil1.mymetod_1 = "Mercedes";
+            bil1.mymetod_2 = 2023;
+
+            Console.WriteLine(" Din bils information " + bil1.mymetod_1+ " Vikten är " + bil1.mymetod_2 + " Årsmodellen är  " + bil1.mymetod_3);
+
+        }
+
+
+    }
+}
